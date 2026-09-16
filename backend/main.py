7947e61dd6c59c519a23192c774f5019fbd9cb38
NehaPanwar5668 <nehapanwar5668@gmail.com>
2026-09-16 10:04:43 +0530

    Fix Vercel CORS

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     "http://127.0.0.1:3000",
      "https://meeting-room-booking-mauve.vercel.app"
     ],
+     allow_origin_regex=r"https://meeting-room-booking-.*\.vercel\.app",
     allow_methods=["*"],
     allow_headers=["*"]
 )
