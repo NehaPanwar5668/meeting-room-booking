d0baf7b4f295653e0b28cb339b67aceb5196246d
NehaPanwar5668 <nehapanwar5668@gmail.com>
2026-09-16 09:11:27 +0530

    Fixing CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
      "https://meeting-room-booking-mauve.vercel.app"
     ],
     allow_methods=["*"],
